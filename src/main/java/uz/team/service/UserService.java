6c16ef03e92c1b6584ef5f49286f7f995252b234
Nodirbek001 <suyunovnodir22@gmail.com>
2022-08-06 00:28:29 +0500

    domain

--- src/main/java/uz/team/service/UserService.java
+++ src/main/java/uz/team/service/UserService.java
@@ -2,13 +2,19 @@ package uz.team.service;
 
 import uz.team.dao.UserDAO;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
 
-public class UserService extends Service<UserDAO >{
+public class UserService extends Service<UserDAO> {
     protected UserService(UserDAO dao) {
         super(dao);
     }
-    public void create(HttpServlet req){
+
+    public void create(HttpServletRequest req) throws ServletException, IOException {
+        Part file = req.getPart("file");
 
     }
 }
