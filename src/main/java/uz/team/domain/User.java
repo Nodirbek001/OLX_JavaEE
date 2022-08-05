3ff9a8f587315c742622ddcbac428afc1cd624cb
Nodirbek001 <suyunovnodir22@gmail.com>
2022-08-05 23:04:52 +0500

    domain

--- src/main/java/uz/team/domain/User.java
+++ src/main/java/uz/team/domain/User.java
@@ -1,9 +1,6 @@
 package uz.team.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -20,5 +17,6 @@ public class User {
     private String email;
     private String username;
     private String password;
+    @OneToOne
     private Card card;
 }
