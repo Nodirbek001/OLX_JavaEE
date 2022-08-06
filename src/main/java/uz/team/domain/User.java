1dfce9716aedbd5fd498a10e03de0e75d66c8571
baxtiyor <abdurashidovbaxtiyor00@gmail.com>
2022-08-06 00:08:58 -0400

    111

--- src/main/java/uz/team/domain/User.java
+++ src/main/java/uz/team/domain/User.java
@@ -9,14 +9,21 @@ import lombok.*;
 @ToString
 @Getter
 @Setter
+@Builder
 
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
+    private String fullName;
     private String username;
     private String password;
+    private Role role;
     @OneToOne
     private Card card;
+
+    public enum Role {
+        ADMIN, USER;
+    }
 }
