efcbfef3b5974cad48fc6536a3d4cfe3925c6ef2
baxtiyor <abdurashidovbaxtiyor00@gmail.com>
2022-08-06 00:05:08 -0400

    111

--- src/main/java/uz/team/dto/user/LoginDTO.java
+++ src/main/java/uz/team/dto/user/LoginDTO.java
@@ -1,4 +1,16 @@
 package uz.team.dto.user;
 
+import uz.team.domain.User;
+import uz.team.util.Utils;
+
 public class LoginDTO {
+    private String username;
+    private String password;
+
+    public User toUser(){
+        return User.builder()
+                .username(this.username)
+                .password(Utils.encodePassword(password))
+                .build();
+    }
 }
---
+++ src/main/java/uz/team/util/Utils.java
@@ -0,0 +1,48 @@
+package uz.team.util;
+
+import lombok.NonNull;
+import org.mindrot.jbcrypt.BCrypt;
+
+
+public class Utils {
+    public static String COVER_EXTENSION = "jpg";
+    public static final String COVER_CONTENT_TYPE = "image/" + COVER_EXTENSION;
+
+    public static boolean isDigit(@NonNull String character) {
+        if (character.isBlank())
+            return false;
+        for (char ch : character.toCharArray()) {
+            if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String generateUniqueFileName() {
+        return generateSerialId() + ".pdf";
+    }
+
+    public static String generateUniqueCoverName() {
+        return generateSerialId() + "." + COVER_EXTENSION;
+    }
+
+    public static String getCoverFileName(@NonNull String filename) {
+        return filename.substring(0, filename.indexOf(".")) + "." + COVER_EXTENSION;
+    }
+
+    private static String generateSerialId() {
+        return "" + System.currentTimeMillis();
+    }
+
+
+    public static String encodePassword(@NonNull final String plainPassword) {
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+    }
+
+    public static boolean matchPassword(@NonNull final String plainPassword, @NonNull final String encodedPassword) {
+        return BCrypt.checkpw(plainPassword, encodedPassword);
+    }
+
+
+}
