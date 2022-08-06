4dbff369d32a0143def280b2f56a0ca842789855
baxtiyor <abdurashidovbaxtiyor00@gmail.com>
2022-08-05 21:05:02 -0400

    111

--- src/main/java/uz/team/severlet/MainServlet.java
+++ src/main/java/uz/team/severlet/MainServlet.java
@@ -8,9 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet(value = "/")
-public class Mainsever extends HttpServlet {
+public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/views/main/admin/AdminPage.jsp").forward(req,resp);
+        req.getRequestDispatcher("/views/main/Main.jsp").forward(req,resp);
     }
 }
--- src/main/webapp/views/main/user/user_page.jsp
+++ src/main/webapp/views/main/user/user_page.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-
+<h2>Hello User</h2>
 </body>
 </html>
